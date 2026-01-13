c5d5b6fe1526c944ddd78fb2ab6267a9b09c634c
bayazid163 <bbsurid2@gmail.com>
2026-01-13 19:23:09 +0600

    Connected Render PostgreSQL

--- somagom/settings.py
+++ somagom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,10 +137,11 @@ WSGI_APPLICATION = 'somagom.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        
+        default='postgresql://somagom_db_user:LBULmif3nphGiiGPL9WeDYVjM4DxIyrO@dpg-d5j48nvgi27c73emseu0-a.virginia-postgres.render.com/somagom_db',
+        conn_max_age=600
+    )
 }
 
 
